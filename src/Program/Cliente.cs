c277882a8cd07d81d1828f5cd4b330ec3f9ce0b2
JoseCarlos2605 <josecarlos.gf26@gmail.com>
2025-10-20 21:45:39 -0300

    Comentarios
    se añaden comentarios

--- src/Program/Cliente.cs
+++ src/Program/Cliente.cs
@@ -2,6 +2,7 @@ namespace a;
 
 public class Cliente : IUsuario
 {
+    //Clase Cliente
     public string Name {get; set;}
     public string Surname { get; set; }
     public string numTelefono { get; set; }
