8c4125c30564de3531b66bf8091b3b09483d09e4
JoseCarlos2605 <josecarlos.gf26@gmail.com>
2025-10-20 21:30:34 -0300

    Metodos y propiedades de cliente
    Se crearon los metodos y propiedades de la clase cliente

--- src/Program/Cliente.cs
+++ src/Program/Cliente.cs
@@ -1,6 +1,30 @@
 namespace a;
 
-public class Cliente
+public class Cliente : IUsuario
 {
-    
+    public string Name {get; set;}
+    public string Surname { get; set; }
+    public string numTelefono { get; set; }
+    public string Mail { get; set; }
+    public Tuple <string, string> Contact { get; set; }
+    public List<string> Inbox { get; set; }
+    public List<string> HistoryMsg{ get; set; }
+    public Vendedor UnVendedor { get; set; }
+
+    public Cliente(string name, string surname, string numTelefono, string mail, Vendedor unVendedor)
+    {
+        this.Name = name;
+        this.Surname = surname;
+        this.Contact = new Tuple<string, string>(numTelefono, mail);
+        this.UnVendedor = unVendedor;
+        this.numTelefono = numTelefono;
+        this.Mail = mail;
+    }
+
+    public void EnviarMsg(string msg)
+    {
+        CRM.SendMsg(this,this.UnVendedor,msg);
+    }
+
+
 }
