cc6af0ff83eedd8078c93c5b05c6412762057eab
JoseCarlos2605 <josecarlos.gf26@gmail.com>
2025-10-20 21:41:53 -0300

    Vendedor
    Se pushea la clase vendedor

--- src/Program/Vendedor.cs
+++ src/Program/Vendedor.cs
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-namespace a;
-
-public class Cliente : IUsuario
-{
-    public string Name {get; set;}
-    public string Surname { get; set; }
-    public string numTelefono { get; set; }
-    public string Mail { get; set; }
-    public Tuple <string, string> Contact { get; set; }
-    public List<string> Inbox { get; set; }
-    public List<string> HistoryMsg{ get; set; }
-    public Vendedor UnVendedor { get; set; }
-
-    public Cliente(string name, string surname, string numTelefono, string mail, Vendedor unVendedor)
-    {
-        this.Name = name;
-        this.Surname = surname;
-        this.Contact = new Tuple<string, string>(numTelefono, mail);
-        this.UnVendedor = unVendedor;
-        this.numTelefono = numTelefono;
-        this.Mail = mail;
-    }
-
-    public void EnviarMsg(string msg)
-    {
-        CRM.SendMsg(this,this.UnVendedor,msg);
-    }
-
-
-}
-=======
 namespace DefaultNamespace;
 
 public class Vendedor: IUsuario
@@ -95,4 +63,3 @@ public class Vendedor: IUsuario
         }
         //--------------------------------------------------//      
     }
->>>>>>> Vendedor
